2a8d4488a41c423b865b372209c0eeba70150e08
jerryliau <125791264+jerryliau@users.noreply.github.com>
2023-06-15 19:32:09 +0800

    Update connect_to_db.py

--- docker/app/functions/connect_to_db.py
+++ docker/app/functions/connect_to_db.py
@@ -3,9 +3,9 @@ import pymysql
 class SQLcommand:
   conn = pymysql.connect(
     host = 'db',
-    database = 'chi101',
-    user = 'chi101',
-    password = 'chi101',
+    database = 'database',
+    user = 'user',
+    password = 'passwd',
     charset = 'utf8mb4'
   )
 
